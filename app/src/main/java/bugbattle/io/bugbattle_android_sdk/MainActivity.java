ec62f3ea59430e443a49f24402e4906080a6a07d
niklasfessler <niklas@boehlerbrothers.com>
2019-01-25 10:53:05 +0100

    added

--- app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
+++ app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
@@ -3,12 +3,20 @@ package bugbattle.io.bugbattle_android_sdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import bugbattle.io.bugbattle.BugBattle;
+import bugbattle.io.bugbattle.BugBattleActivationMethod;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        BugBattle.initialize("", BugBattleActivationMethod.SHAKE, MainActivity.this);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        BugBattle.resume();
+    }
 }
