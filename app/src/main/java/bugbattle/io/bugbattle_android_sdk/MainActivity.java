e4437ea128de30780fdf50bd687e14e78069e69c
Niklas <nfe3303@students.fhv.at>
2019-02-05 11:02:26 +0100

    cleanup

--- app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
+++ app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
@@ -3,15 +3,10 @@ package bugbattle.io.bugbattle_android_sdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import bugbattle.io.bugbattle.BugBattle;
-import bugbattle.io.bugbattle.BugBattleActivationMethod;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BugBattle.initialise(this,"5c41fcd794aaf5331dd4d513", BugBattleActivationMethod.SHAKE);
-        BugBattle.trigger();
     }
 }
