675808e80d766ae01787d5f85a514fc2d4c6c6d4
niklasfessler <niklas@boehlerbrothers.com>
2019-02-13 10:34:11 +0100

    fixed multi-screen bug

--- app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
+++ app/src/main/java/bugbattle/io/bugbattle_android_sdk/MainActivity.java
@@ -1,7 +1,10 @@
 package bugbattle.io.bugbattle_android_sdk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import bugbattle.io.bugbattle.BugBattle;
 import bugbattle.io.bugbattle.BugBattleActivationMethod;
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BugBattle.initialise(this,"5c41fcd794aaf5331dd4d513", BugBattleActivationMethod.SHAKE);
+        BugBattle.initialise(this, "5c41fcd794aaf5331dd4d513", BugBattleActivationMethod.SHAKE);
+
     }
 }
--- bugbattle/src/main/java/bugbattle/io/bugbattle/ScreenshotTaker.java
+++ bugbattle/src/main/java/bugbattle/io/bugbattle/ScreenshotTaker.java
@@ -1,22 +1,28 @@
 package bugbattle.io.bugbattle;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.view.View;
 
+import java.lang.reflect.Field;
 import java.util.Date;
+import java.util.Map;
 
- class ScreenshotTaker {
-    private Activity activity;
+class ScreenshotTaker {
+   // private Activity activity;
     public ScreenshotTaker(Activity currActivity) {
-        activity = currActivity;
+  //      activity = currActivity;
         service = FeedbackService.getInstance();
 
     }
@@ -35,11 +41,67 @@ import java.util.Date;
          return returnedBitmap;
      }
 
-    public void takeScreenshot() throws Exception {
+     public static Bitmap takeScreenShot(Activity activity) {
+         View view = activity.getWindow().getDecorView();
+         view.setDrawingCacheEnabled(true);
+         view.buildDrawingCache();
+         Bitmap b1 = view.getDrawingCache();
+         Rect frame = new Rect();
+         activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+         int statusBarHeight = frame.top;
+
+         //Find the screen dimensions to create bitmap in the same size.
+         int width = activity.getWindowManager().getDefaultDisplay().getWidth();
+         int height = activity.getWindowManager().getDefaultDisplay().getHeight();
+
+         Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height - statusBarHeight);
+         view.destroyDrawingCache();
+         return b;
+     }
+
+
+     public static Activity getActivity() {
+         try {
+             Class activityThreadClass = Class.forName("android.app.ActivityThread");
+             Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+             Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+             activitiesField.setAccessible(true);
+
+             Map<Object, Object> activities = (Map<Object, Object>) activitiesField.get(activityThread);
+             if (activities == null)
+                 return null;
+
+             for (Object activityRecord : activities.values()) {
+                 Class activityRecordClass = activityRecord.getClass();
+                 Field pausedField = activityRecordClass.getDeclaredField("paused");
+                 pausedField.setAccessible(true);
+                 if (!pausedField.getBoolean(activityRecord)) {
+                     Field activityField = activityRecordClass.getDeclaredField("activity");
+                     activityField.setAccessible(true);
+                     Activity activity = (Activity) activityField.get(activityRecord);
+                     return activity;
+                 }
+             }
+
+             return null;
+         }catch (Exception e) {
+
+
+         }
+         return null;
+     }
+
+     public void takeScreenshot() throws Exception {
+     /*    ActivityManager am = (ActivityManager)activity.getSystemService(Context.ACTIVITY_SERVICE);
+         ActivityManager.RunningTaskInfo cn = am.getRunningTasks(1).get(0);
+         */
+
+
         // create bitmap screen capture
-        View v1 = activity.getWindow().getDecorView().getRootView();
+        View v1 = getActivity().getWindow().getDecorView().getRootView() ;
         v1.setDrawingCacheEnabled(true);
-        Bitmap bitmap = loadBitmapFromView(activity, v1);
+        Bitmap bitmap = v1.getDrawingCache();
+
 
         bitmap = getResizedBitmap(bitmap);
         openScreenshot(bitmap);
